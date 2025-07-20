2d1ce4f3b7a11178f54f74dd77007e35fa1a07db
HaikalFras <haikalfrastiawan16@gmail.com>
2025-07-20 12:05:16 +0700

    Create Garage.jsx

---
+++ component/Garage.jsx
@@ -0,0 +1,13 @@
+function Garage (props){
+    const cars = props.cars 
+    return (
+        <>
+        <h1>Garage</h1>
+      {cars.length > 0 && <h2>you have {cars.length} cars in your garage</h2>}
+        </>
+    )
+}
+
+
+
+export default Garage
