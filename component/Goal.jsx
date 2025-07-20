5e63b811f813d661080d63bd33a7a157f83d10ce
HaikalFras <haikalfrastiawan16@gmail.com>
2025-07-20 12:04:47 +0700

    Update Goal.jsx

--- component/Goal.jsx
+++ component/Goal.jsx
@@ -6,15 +6,29 @@ function MissedGoal() {
 function MadeGoald (){
     return <h2>Your goalll!!!</h2>
 }
+// if stetmen
+// function Goal(props){
+//     const isGoal = props.isGoal
 
-function Goal(props){
-    const isGoal = props.isGoal
-
-    if(isGoal){
-        return <MadeGoald />
-    }
-        return <MissedGoal />
+//     if(isGoal){
+//         return <MadeGoald />
+//     }
+//         return <MissedGoal />
     
+// }
+
+
+
+
+//ternary operation
+function Goal(props){
+     const isGoal = props.isGoal
+     return(
+        <>
+        {isGoal ? <MadeGoald /> :<MissedGoal />}
+        </>
+        
+     )
 }
 
 export default Goal
