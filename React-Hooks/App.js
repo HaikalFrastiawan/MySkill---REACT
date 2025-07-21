d42233a0f9f7de6c1de2763d3e5d2b1f02187037
HaikalFras <haikalfrastiawan16@gmail.com>
2025-07-21 21:32:26 +0700

    Update App.js

--- React-Hooks/App.js
+++ React-Hooks/App.js
@@ -1,1 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import FavoritColor from './pages/FavoritCalor';
+import Car from './pages/Car';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      <FavoritColor />
+      <Car />
+    </div>
+  );
+}
+
+export default App;
 
