9481bfb4ca5900f55c72efb67cb02fbf65c09772
HaikalFras <haikalfrastiawan16@gmail.com>
2025-07-22 07:34:19 +0700

    Create Form.js

---
+++ react-form/pages/Form.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function MyForm (){
+    return(
+        <>
+        <form>
+            <label>Masukan nama kamu ini tanpa event handler</label>
+            <input type="text"/>
+            <input type="submit"/>
+
+
+
+        </form>
+        
+        </>
+    )
+}
+
+export default MyForm
