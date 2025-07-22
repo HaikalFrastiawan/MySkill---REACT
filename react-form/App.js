ca95c6a9fe72400342adfbff9fcd1f3672c93196
HaikalFras <haikalfrastiawan16@gmail.com>
2025-07-22 07:35:08 +0700

    Update App.js

--- react-form/App.js
+++ react-form/App.js
@@ -1,1 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import MyForm from './page/Form';
+import MyForm2 from './page/Form-Handle';
+
+function App() {
+  return (
+    <div className="App">
+      <MyForm />
+      <MyForm2  />
+    </div>
+  );
+}
+
+export default App;
 
